8f9d4487f1271c4f77f39bf40accc84ec771f492
OJP98 <ojuarez_p@hotmail.com>
2018-03-21 10:20:02 -0600

    Implementando clases

---
+++ HojaDeTrabajo8/src/Principal.java
@@ -0,0 +1,31 @@
+/**
+ * Clase que realiza las interacciones con el usuario.
+ * Algoritmos y estructura de datos - seccion 10.
+ * @author: Oscar Juarez - 17315
+ * @author: Guillermo Sandoval - 17577
+ * @version: 23/03/18
+ */
+
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        boolean power = true;
+        int decision;
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Bienvenido al programa!\n");    
+        
+        while (power) {
+            
+            System.out.println("Ingrese ");
+            
+            
+        }
+        
+        
+    }
+    
+}
